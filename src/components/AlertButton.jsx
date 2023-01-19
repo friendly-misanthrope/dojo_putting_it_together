e08847854ebaa06c6a48ddfe9d19b9b844443a13
NickBarkerDev <nickthedevguy@gmail.com>
2023-01-19 08:09:07 -0800

    cleaned up comments

--- src/components/AlertButton.jsx
+++ src/components/AlertButton.jsx
@@ -1,9 +1,5 @@
 import React from 'react'
 
-// I'm trying to figure out how to
-// force prompt() to trigger after a third
-// invalid input or upon the user clicking 'cancel' in the prompt.
-
 const sayName = (yourName = prompt("What's your name?")) => {
     if (yourName) {
         return `Hello, ${yourName}! Warm greetings from your friendly desktop robot!`;
